e16fa567456b9d0ab5a4da18385b4ab3342607cc
Kurt Aquino <kurtaquino82@gmail.com>
2021-10-22 19:01:48 +0800

    Reorganized Model Files

--- app/core/models.py
+++ app/core/models.py
@@ -1,40 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-                                        PermissionsMixin
-from django.conf import settings
 
-class UserManager(BaseUserManager):
-
-    def create_user(self, email, password=None, **extra_fields):
-        """Creates and saves a new user"""
-        if not email:
-            raise ValueError('Users must have an email address')
-        user = self.model(email=self.normalize_email(email), **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-
-        return user
-
-    def create_superuser(self, email, password):
-        """Creates and saves a new super user"""
-        user = self.create_user(email, password)
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
-
-        return user
-
-
-class User(AbstractBaseUser, PermissionsMixin):
-    """Custom user model that supports using email instead of username"""
-    email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-
-    objects = UserManager()
-
-    USERNAME_FIELD = 'email'
 
 class BaseInfo(models.Model):
   created_at = models.DateTimeField(auto_now=False, auto_now_add=True, blank=True, null=True)
@@ -44,6 +9,7 @@ class BaseInfo(models.Model):
       ordering = ['-updated_at', '-created_at']
       abstract = True
 
+<<<<<<< Updated upstream
 class Account(BaseInfo):
   username = models.CharField(max_length=155)
   wallet_addr = models.CharField(max_length=155, unique=True)
@@ -160,3 +126,5 @@ class Team(BaseInfo):
     
     class Meta:
         ordering = ['-created_at', '-updated_at']
+=======
+>>>>>>> Stashed changes
