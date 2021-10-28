6b0f32332e56b031cb76aa9867a9923935ad6656
Angelo Matias <geloko@users.noreply.github.com>
2021-10-28 17:35:57 +0800

    added django cors headers

--- app/app/settings.py
+++ app/app/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
     #swagger generator
     'drf_yasg',
 
+    #cors
+    'corsheaders',
+
     #apps
     'core',
     'user',
@@ -62,6 +65,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
@@ -144,6 +149,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
