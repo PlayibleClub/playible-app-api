4f8bf04ca112076976f20bab5ad93a848f71658a
Vince Gonzales <vincegonzalesdlsu@gmail.com>
2022-04-04 13:01:17 +0800

    added terra network and terra endpoint to env

--- apps/core/terra.py
+++ apps/core/terra.py
@@ -6,12 +6,12 @@ from asgiref.sync import async_to_sync
 from rest_framework import serializers
 
 from apps.core import utils
-from config.settings.base import ADMIN_WALLET_MNEMONIC
+from config.settings.base import ADMIN_WALLET_MNEMONIC, TERRA_ENDPOINT, TERRA_NETWORK
 
 
 @async_to_sync
 async def get_latest_block_height():
-    terra = AsyncLCDClient("https://bombay-lcd.terra.dev", "bombay-12")
+    terra = AsyncLCDClient(TERRA_ENDPOINT, TERRA_NETWORK)
     block_height = await terra.tendermint.block_info()
     return block_height['block']['header']['height']
 
@@ -19,7 +19,7 @@ async def get_latest_block_height():
 @async_to_sync
 async def get_tx_info(tx_hash):
     try:
-        terra = AsyncLCDClient("https://bombay-lcd.terra.dev", "bombay-12")
+        terra = AsyncLCDClient(TERRA_ENDPOINT, TERRA_NETWORK)
         tx_info = await terra.tx.tx_info(tx_hash)
         return tx_info
     except:
@@ -29,7 +29,7 @@ async def get_tx_info(tx_hash):
 @async_to_sync
 async def query_contract(contract_addr, query_msg):
     try:
-        terra = AsyncLCDClient("https://bombay-lcd.terra.dev", "bombay-12")
+        terra = AsyncLCDClient(TERRA_ENDPOINT, TERRA_NETWORK)
         response = await terra.wasm.contract_query(contract_addr, query_msg)
         await terra.session.close()
         return response
@@ -41,7 +41,7 @@ async def query_contract(contract_addr, query_msg):
 async def create_and_sign_tx(msgs):
     try:
         mk = MnemonicKey(mnemonic=ADMIN_WALLET_MNEMONIC)
-        terra = AsyncLCDClient("https://bombay-lcd.terra.dev", "bombay-12")
+        terra = AsyncLCDClient(TERRA_ENDPOINT, TERRA_NETWORK)
         wallet = terra.wallet(mk)
         tx = await wallet.create_and_sign_tx(
             CreateTxOptions(
